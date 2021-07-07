07d3c5f941dda3f30bb1071ff3c8c533aeb07810
NiranjanBelambe <67544041+NiranjanBelambe@users.noreply.github.com>
2021-07-07 19:19:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -125,7 +125,5 @@ app.get("/logout",(req,res)=>{
 });
 
 
-  app.listen(port, function() {
-    console.log("Server started on port 3000");
-  });
+  app.listen(port,()=>console.info(`Listening on ${port}`))
   
